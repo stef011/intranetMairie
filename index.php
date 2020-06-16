06dab67523f01f944d64613c5cbce4b70f88c89f
Zundel Stéphane <stephane.zundel@viacesi.fr>
2020-06-16 11:44:56 +0200

    Added .env file. Added installation section to readme Added dotEnvLoader package with composer

--- index.php
+++ index.php
@@ -3,6 +3,8 @@ require('./vendor/autoload.php');
 
 use App\Router\Router;
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
 $router = new Router($_GET['url']);
 $router->get('/', 'IndexController@show');
